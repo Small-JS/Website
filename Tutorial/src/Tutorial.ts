941d9dbeb0296f9cf003118d0aff45ddb4046c36
FunctionPoint <FunctionPoint@users.noreply.github.com>
2026-01-03 14:23:07 +0100

    Tutorial: Added navigation to next and previous page #1

--- Tutorial/src/Tutorial.ts
+++ Tutorial/src/Tutorial.ts
@@ -2,6 +2,8 @@
 export class Tutorial
 {
 	menuButton!: HTMLButtonElement;
+	previousButton!: HTMLButtonElement;
+	nextButton!: HTMLButtonElement;
 	indexDiv!: HTMLDivElement;
 	pageDiv!: HTMLDivElement;
 	indexPopup = false;
@@ -17,52 +19,77 @@ export class Tutorial
 
 	async loadIncludes()
 	{
-		const includeNodes = document.getElementsByTagName( 'include' );
+		const includeNodes = document.getElementsByTagName( "include" );
 		for( const includeNode of includeNodes ) {
-			let filePath = <string> includeNode.getAttribute( 'src' );
+			let filePath = <string> includeNode.getAttribute( "src" );
 			let file = await fetch( filePath );
 			let text = await file.text();
-			includeNode.insertAdjacentHTML( 'afterend', text );
+			includeNode.insertAdjacentHTML( "afterend", text );
+			// This might produce a bug with multiple include nodes?
+			includeNode.remove();
 		}
 	}
 
 	bindElements()
 	{
-		this.menuButton = <HTMLButtonElement> document.getElementById( "menuButton" );
-		if( ! this.menuButton )
-			throw new Error( "HTML element not found: menuButton" );
-		this.menuButton.onclick = event => this.onMenuButton()
+		this.menuButton = <HTMLButtonElement> this.getElement( "menuButton", "button" );
+		this.menuButton.onclick = event => this.onMenuButton();
 
-		this.indexDiv = <HTMLDivElement> document.getElementById( "indexDiv" );
-		if( !this.indexDiv )
-			throw new Error( "HTML element not found: indexDiv" );
+		this.menuButton = <HTMLButtonElement> this.getElement( "previousButton", "button" );
+		this.menuButton.onclick = event => this.onPreviousButton();
+		this.menuButton = <HTMLButtonElement> this.getElement( "nextButton", "button" );
+		this.menuButton.onclick = event => this.onNextButton();
 
-		this.pageDiv = <HTMLDivElement> document.getElementById( "pageDiv" );
-		if( !this.pageDiv )
-			throw new Error( "HTML element not found: pageDiv" );
+		this.indexDiv = <HTMLDivElement> this.getElement( "indexDiv", "div" );
+		this.pageDiv = <HTMLDivElement> this.getElement( "pageDiv", "div" );
+	}
+
+	// Get HTML element also id checking for existence and correct tag
+	// Otherwise throw an error.
+
+	getElement( id: string, tagName: string ): HTMLElement
+	{
+		let element = document.getElementById( id );
+		if( ! element )
+			throw new Error( "HTML element id not found: " + id );
+
+		tagName = tagName.toUpperCase();
+		if( element.tagName != tagName )
+			throw new Error( "HTML element tag name unexpected: " + element.tagName +
+				", expected: " + tagName );
+
+		return element;
 	}
 
 	// Open the index tree details to show the entry for the current page.
 
 	openIndexForPage()
 	{
-		// Find entry with same name as page URL
-
-		let url = window.location.pathname;
-		let fileName = url.substring( url.lastIndexOf( '/' ) + 1 );
-		fileName = fileName.substring( 0, fileName.lastIndexOf( '.' ) )
-		let element = document.getElementById( fileName + "Entry" );
-		if( !element )
-			throw new Error( "HTML element not found: " + element );
+		let element: HTMLElement | null = this.currentIndexElement();
 
 		// Open all <detail> parent HTML elements,
 		// until the index div container is reached
 
 		while( element && element != this.indexDiv ) {
-			if( element.nodeName == "DETAILS" )
+			if( element.tagName == "DETAILS" )
 				( <HTMLDetailsElement> element ).open = true;
 			element = element.parentElement;
 		}
+	}
+
+	// Find entry with same name as URL of current page
+	// Throw an error if it is not found.
+
+	currentIndexElement(): HTMLElement
+	{
+		let url = window.location.pathname;
+		let fileName = url.substring( url.lastIndexOf( "/" ) + 1 );
+		fileName = fileName.substring( 0, fileName.lastIndexOf( "." ) )
+		let element = document.getElementById( fileName + "Entry" );
+		if( !element )
+			throw new Error( "HTML element not found: " + element );
+
+		return element;
 	}
 
 	onMenuButton()
@@ -78,6 +105,94 @@ export class Tutorial
 			this.pageDiv.style.display = "none";
 			this.indexPopup = true;
 		}
+	}
+
+	onPreviousButton()
+	{
+		let element = this.currentIndexElement();
+		this.navigateIntoPrevious( element );
+	}
+
+	// Find the previous "sibling" in the tree and navigate into last link
+
+	navigateIntoPrevious( element: Element | null ): undefined
+	{
+		if( ! element )
+			throw Error( "Failed to find previous sibling element in tree" );
+
+		let previousElement = element.previousElementSibling;
+
+		while( previousElement && previousElement.tagName == "BR" )
+			previousElement = previousElement.previousElementSibling;
+
+		if( ! previousElement ) {
+			if( element == this.indexDiv )
+				return;
+			return this.navigateIntoPrevious( element.parentElement );
+		}
+
+		this.navigateIntoLast( previousElement );
+	}
+
+	navigateIntoLast( element: Element | null ): undefined
+	{
+		if( ! element )
+			throw new Error( "Failed to find last anchor element" );
+
+		if( element.tagName == "SUMMARY" || element.tagName == "DETAILS" )
+			return this.navigateIntoLast( element.lastElementChild )
+
+		while( element && element.tagName == "BR" )
+			element = element.previousElementSibling;
+
+		if( ! element )
+			throw new Error( "Failed to find last anchor element" );
+
+		if( element.tagName != "A" )
+			throw new Error( "Unexpected tag: " + element.tagName );
+
+		window.location.href = ( <HTMLAnchorElement> element ).href;
+	}
+
+	onNextButton()
+	{
+		let element = this.currentIndexElement();
+		this.navigateIntoNext( element );
+	}
+
+	// Find the next "sibling" in the tree and navigate into first link
+
+	navigateIntoNext( element: Element | null ): undefined
+	{
+		if( ! element )
+			throw Error( "Failed to find next sibling element in tree" );
+
+		let nextElement = element.nextElementSibling;
+
+		while( nextElement && nextElement.tagName == "BR" )
+			nextElement = nextElement.nextElementSibling;
+
+		if( ! nextElement ) {
+			if( element == this.indexDiv )
+				return;
+			return this.navigateIntoNext( element.parentElement );
+		}
+
+		this.navigateIntoFirst( nextElement );
+	}
+
+	navigateIntoFirst( element: Element | null ): undefined
+	{
+		if( ! element )
+			throw new Error( "Failed to find next first anchor element" );
+
+		if( element.tagName == "SUMMARY" || element.tagName == "DETAILS" )
+			return this.navigateIntoFirst( element.firstElementChild )
+
+		if( element.tagName != "A" )
+			throw new Error( "Unexpected tag: " + element.tagName );
+
+		window.location.href = ( <HTMLAnchorElement> element ).href;
 	}
 
 }
--- Tutorial/web/Tutorial/Tutorial/Script/Tutorial.js
+++ Tutorial/web/Tutorial/Tutorial/Script/Tutorial.js
@@ -1,5 +1,7 @@
 export class Tutorial {
     menuButton;
+    previousButton;
+    nextButton;
     indexDiv;
     pageDiv;
     indexPopup = false;
@@ -10,42 +12,59 @@ export class Tutorial {
     }
     // Replaces tags <include src="<file path>"> with referenced HTML
     async loadIncludes() {
-        const includeNodes = document.getElementsByTagName('include');
+        const includeNodes = document.getElementsByTagName("include");
         for (const includeNode of includeNodes) {
-            let filePath = includeNode.getAttribute('src');
+            let filePath = includeNode.getAttribute("src");
             let file = await fetch(filePath);
             let text = await file.text();
-            includeNode.insertAdjacentHTML('afterend', text);
+            includeNode.insertAdjacentHTML("afterend", text);
+            // This might produce a bug with multiple include nodes?
+            includeNode.remove();
         }
     }
     bindElements() {
-        this.menuButton = document.getElementById("menuButton");
-        if (!this.menuButton)
-            throw new Error("HTML element not found: menuButton");
+        this.menuButton = this.getElement("menuButton", "button");
         this.menuButton.onclick = event => this.onMenuButton();
-        this.indexDiv = document.getElementById("indexDiv");
-        if (!this.indexDiv)
-            throw new Error("HTML element not found: indexDiv");
-        this.pageDiv = document.getElementById("pageDiv");
-        if (!this.pageDiv)
-            throw new Error("HTML element not found: pageDiv");
+        this.menuButton = this.getElement("previousButton", "button");
+        this.menuButton.onclick = event => this.onPreviousButton();
+        this.menuButton = this.getElement("nextButton", "button");
+        this.menuButton.onclick = event => this.onNextButton();
+        this.indexDiv = this.getElement("indexDiv", "div");
+        this.pageDiv = this.getElement("pageDiv", "div");
+    }
+    // Get HTML element also id checking for existence and correct tag
+    // Otherwise throw an error.
+    getElement(id, tagName) {
+        let element = document.getElementById(id);
+        if (!element)
+            throw new Error("HTML element id not found: " + id);
+        tagName = tagName.toUpperCase();
+        if (element.tagName != tagName)
+            throw new Error("HTML element tag name unexpected: " + element.tagName +
+                ", expected: " + tagName);
+        return element;
     }
     // Open the index tree details to show the entry for the current page.
     openIndexForPage() {
-        // Find entry with same name as page URL
-        let url = window.location.pathname;
-        let fileName = url.substring(url.lastIndexOf('/') + 1);
-        fileName = fileName.substring(0, fileName.lastIndexOf('.'));
-        let element = document.getElementById(fileName + "Entry");
-        if (!element)
-            throw new Error("HTML element not found: " + element);
+        let element = this.currentIndexElement();
         // Open all <detail> parent HTML elements,
         // until the index div container is reached
         while (element && element != this.indexDiv) {
-            if (element.nodeName == "DETAILS")
+            if (element.tagName == "DETAILS")
                 element.open = true;
             element = element.parentElement;
         }
+    }
+    // Find entry with same name as URL of current page
+    // Throw an error if it is not found.
+    currentIndexElement() {
+        let url = window.location.pathname;
+        let fileName = url.substring(url.lastIndexOf("/") + 1);
+        fileName = fileName.substring(0, fileName.lastIndexOf("."));
+        let element = document.getElementById(fileName + "Entry");
+        if (!element)
+            throw new Error("HTML element not found: " + element);
+        return element;
     }
     onMenuButton() {
         // If the index is popup mode, hide it again and show the page
@@ -59,6 +78,64 @@ export class Tutorial {
             this.pageDiv.style.display = "none";
             this.indexPopup = true;
         }
+    }
+    onPreviousButton() {
+        let element = this.currentIndexElement();
+        this.navigateIntoPrevious(element);
+    }
+    // Find the previous "sibling" in the tree and navigate into last link
+    navigateIntoPrevious(element) {
+        if (!element)
+            throw Error("Failed to find previous sibling element in tree");
+        let previousElement = element.previousElementSibling;
+        while (previousElement && previousElement.tagName == "BR")
+            previousElement = previousElement.previousElementSibling;
+        if (!previousElement) {
+            if (element == this.indexDiv)
+                return;
+            return this.navigateIntoPrevious(element.parentElement);
+        }
+        this.navigateIntoLast(previousElement);
+    }
+    navigateIntoLast(element) {
+        if (!element)
+            throw new Error("Failed to find last anchor element");
+        if (element.tagName == "SUMMARY" || element.tagName == "DETAILS")
+            return this.navigateIntoLast(element.lastElementChild);
+        while (element && element.tagName == "BR")
+            element = element.previousElementSibling;
+        if (!element)
+            throw new Error("Failed to find last anchor element");
+        if (element.tagName != "A")
+            throw new Error("Unexpected tag: " + element.tagName);
+        window.location.href = element.href;
+    }
+    onNextButton() {
+        let element = this.currentIndexElement();
+        this.navigateIntoNext(element);
+    }
+    // Find the next "sibling" in the tree and navigate into first link
+    navigateIntoNext(element) {
+        if (!element)
+            throw Error("Failed to find next sibling element in tree");
+        let nextElement = element.nextElementSibling;
+        while (nextElement && nextElement.tagName == "BR")
+            nextElement = nextElement.nextElementSibling;
+        if (!nextElement) {
+            if (element == this.indexDiv)
+                return;
+            return this.navigateIntoNext(element.parentElement);
+        }
+        this.navigateIntoFirst(nextElement);
+    }
+    navigateIntoFirst(element) {
+        if (!element)
+            throw new Error("Failed to find next first anchor element");
+        if (element.tagName == "SUMMARY" || element.tagName == "DETAILS")
+            return this.navigateIntoFirst(element.firstElementChild);
+        if (element.tagName != "A")
+            throw new Error("Unexpected tag: " + element.tagName);
+        window.location.href = element.href;
     }
 }
 //# sourceMappingURL=Tutorial.js.map
